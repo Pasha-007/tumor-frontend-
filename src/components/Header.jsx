25d4a6fbf317e9ea72ddf314d15fcf08d3ef305b
Mohammed Muntahaa Khan <mohammedmuntahaa.khan@alatoo.edu.kg>
2025-03-10 17:41:54 +0600

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 const Header = () => {
-    return (
-      <header className="header">
-        <h1>Brain Tumor Classification</h1>
-      </header>
-    );
-  };
-  
-  export default Header;
+  return (
+    <header className="header">
+      <nav className="nav">
+        <h1 className="title">Brain Tumor Classifier</h1>
+        <ul className="nav-links">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+export default Header;
